0a6c0933dd1f4ede917c63a187bcb22f55d74c97
YYjoke <895479558@qq.com>
2019-08-06 22:20:53 +0800

    教程代码

---
+++ Vuecode/03day/test.php
@@ -0,0 +1,8 @@
+<?php
+header('Content-type:text/json');
+$str = '[{"id":"1","name":"penghui1","age":"20","phone":"15839922830"},
+         {"id":"2","name":"penghui2","age":"21","phone":"15839922830"},
+         {"id":"3","name":"penghui3","age":"22","phone":"15839922830"},
+         {"id":"4","name":"penghui4","age":"23","phone":"15839922830"}]';
+echo $str;
+ ?>
